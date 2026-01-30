5f08614536739cf19930cce73a0431feea904c90
Sanketmate <sanketmate2911@gmail.com>
2026-01-30 11:23:37 +0530

    practicing function expression in js

---
+++ Javascript/Functions/Functions_with_Expressions/Assignment4.js
@@ -0,0 +1,37 @@
+/* function hello() {
+  console.log("Hello");
+  converting to func expression
+} */
+
+let hello = function () {
+    console.log("Hello")
+}
+
+hello()
+
+cube = function (n) {
+    return n*n*n;    
+}
+
+a=cube(2)
+console.log(a)
+
+sayHi = function () {
+    console.log("Hi")
+}
+
+sayHi()
+
+add = function (a,b) {
+    return a+b
+}
+
+
+a=add(2,5)
+console.log(a)
+
+multiple = function(a,b) {
+    console.log(a*b)
+}
+
+multiple(2,3)
