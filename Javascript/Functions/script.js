e3c8c9f6c84d5e2919e1688cf8dc94a232a11fef
Sanketmate <sanketmate2911@gmail.com>
2026-01-30 10:21:04 +0530

    added comment's for better reading

--- Javascript/Functions/script.js
+++ Javascript/Functions/script.js
@@ -1,16 +1,16 @@
 function sayHello() {
     console.log("Hello, Welcome")
 }
-sayHello()
+sayHello() // This function print "Hello, Welcome" on console
 
 function add(a,b) {
     console.log(a+b);
 }
-add(5,10)
+add(5,10) // This Function adds & print sum on console
 
 function add2(a,b) {
-    return (a+b)
+    return (a+b) // This functions returns the sum of 2 numbers;
 }
 
-sum = add2(5,11)
-console.log(sum)
+sum = add2(5,11) // This functions get the sum of 2 numbers
+console.log(sum) //Print the sum of 2 numbers.
