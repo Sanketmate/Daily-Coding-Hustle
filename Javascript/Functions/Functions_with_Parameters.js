1c5b08405aace20a9c3bb6f0c1380108e362a4b8
Sanketmate <sanketmate2911@gmail.com>
2026-01-30 10:43:35 +0530

    2nd commit

--- Javascript/Functions/Functions_with_Parameters.js
+++ Javascript/Functions/Functions_with_Parameters.js
@@ -4,4 +4,34 @@ function square(numbers) {
 
 
 result= square(2)
-console.log(result)
+console.log(result) // This functions return square of functions
+
+function findArea(lenght,width) {
+    return lenght*width;
+}
+
+result = findArea(2,5);
+console.log(result) // Area
+
+function isEven(numbers) {
+    if (numbers%2==0) {
+        console.log("Even")
+    }else{
+        console.log("Odd")
+    }
+}
+
+isEven(3)
+
+function sum(a,b) {
+    return a+b;
+}
+sum(1,2)
+r=sum(3,3)
+console.log(r)
+console.log(sum)
+
+function displaySum(a,b) {
+    console.log(a+b)
+}
+displaySum(3,5)
