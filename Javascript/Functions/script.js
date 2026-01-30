8fa105f38f8b6d96027584085799f059b893f3aa
Sanketmate <sanketmate2911@gmail.com>
2026-01-30 10:14:52 +0530

    created 2 new function  1st which print sum & 2nd which return sum

--- Javascript/Functions/script.js
+++ Javascript/Functions/script.js
@@ -1,5 +1,16 @@
 function sayHello() {
     console.log("Hello, Welcome")
 }
-
 sayHello()
+
+function add(a,b) {
+    console.log(a+b);
+}
+add(5,10)
+
+function add2(a,b) {
+    return (a+b)
+}
+
+sum = add2(5,11)
+console.log(sum)
