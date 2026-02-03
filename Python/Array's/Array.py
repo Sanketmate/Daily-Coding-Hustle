7f692018c265c5e3c4f6548f30b75c29c84fd2ed
Sanketmate <sanketmate2911@gmail.com>
2026-02-03 15:34:13 +0530

    Wrote the array's program in Java

---
+++ Python/Array's/Array.py
@@ -0,0 +1,11 @@
+# Create an array (list) of 5 fruits
+fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"]
+
+# Print the first fruit
+print("First fruit:", fruits[0])
+
+# Print the last fruit
+print("Last fruit:", fruits[-1])
+
+# Print the length of the array
+print("Length of the array:", len(fruits))
