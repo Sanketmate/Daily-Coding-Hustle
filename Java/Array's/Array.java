d540ac49b2d11bd5bbb529c1ebcadffa65f6e303
Sanketmate <sanketmate2911@gmail.com>
2026-02-03 15:25:51 +0530

    Added the folder Java & Python folder

---
+++ Java/Array's/Array.java
@@ -0,0 +1,8 @@
+class Array{
+    public static void main(String[] args) {
+        int arr[]={1,2,6,7,8};
+        for (int i =0;i<arr.length;i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
