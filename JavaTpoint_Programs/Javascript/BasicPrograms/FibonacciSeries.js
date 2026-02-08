3a0f72bc48880a194c59db6ad2be06d31d92eb14
Sanketmate <sanketmate2911@gmail.com>
2026-02-08 21:38:24 +0530

    added book

--- JavaTpoint_Programs/Javascript/BasicPrograms/FibonacciSeries.js
+++ JavaTpoint_Programs/Javascript/BasicPrograms/FibonacciSeries.js
@@ -2,7 +2,15 @@ let n = 10
 
 let a = 0, b = 1;
 
-for (let i = 0; i < n; i++) {
-  console.log(a);
-  [a, b] = [b, a + b];
+// for (let i = 0; i < n; i++) {
+//   console.log(a);
+//   [a, b] = [b, a + b];
+// }
+
+for (let i = 1; i <= n; i++) 
+{
+    console.log(a + " ")
+    let sum = a + b;
+    a = b;
+    b = sum;
 }
