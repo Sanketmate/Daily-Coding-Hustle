3337036c6c56bc6aa5caa98f0b1d2530ee841b3b
Sanketmate <sanketmate2911@gmail.com>
2026-02-11 12:05:11 +0530

    Wrote the PasCal Triangle in pyton

---
+++ JavaTpoint_Programs/Python/Patterns/PasCalTriangle.py
@@ -0,0 +1,21 @@
+# PasCal Triange.
+
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+# * * * * 
+# * * *
+# * *
+# *
+
+for i in range(0,6):
+    for j in range(0,i+1):
+        print("*",end=" ")
+    print()
+
+for i in range(5, -1,-1):
+    for j in range(0,i):
+        print("*",end=" ")
+    print()
