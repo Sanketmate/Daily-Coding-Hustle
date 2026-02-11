3495758c9a0424efc8f5045aecf891db10922979
Sanketmate <sanketmate2911@gmail.com>
2026-02-11 11:18:08 +0530

    equi-lateralTriangle.py

---
+++ JavaTpoint_Programs/Python/Patterns/equi-lateralTriangle.py
@@ -0,0 +1,15 @@
+#______*
+#_____* *
+#____* * *
+#___* * * *
+#__* * * * *
+#_* * * * * *
+
+
+for i in range(0,7):
+    for j in range(7,i,-1):
+        print("_",end="")
+    for k in range(0,i+1):
+        print("*",end=" ")
+    print()
+
